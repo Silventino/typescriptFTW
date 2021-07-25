fdbff3fdfe6a8dfb3a084436cdfa5003a544055f
Pedro <silventino.dev@gmail.com>
2021-07-25 00:46:49 -0300

    better front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,21 @@
 import "reflect-metadata";
 
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
 import "./App.css";
-import { User } from "./shared/entity/User";
+import Login from "./components/Login";
+import SignIn from "./components/SignIn";
+
 
 function App() {
-  const user = new User();
-
+  const [login, setLogin] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        login ?
+        <Login />
+        :
+        <SignIn/>
+      }
     </div>
   );
 }
